572e350ff9ab8e391352f65f880b1228aed5c4bc
sunchuan <272420199@qq.com>
2020-01-19 17:35:49 +0800

    【Mybatis+spring整合源码探秘】--- mybatis整合spring事务原理

--- mybatis-spring/src/main/java/com/nrsc/mybatis/Service/impl/TUserServiceImpl.java
+++ mybatis-spring/src/main/java/com/nrsc/mybatis/Service/impl/TUserServiceImpl.java
@@ -5,6 +5,7 @@ import com.nrsc.mybatis.domian.TUser;
 import com.nrsc.mybatis.mapper.TUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /***
  * @author : Sun Chuan
@@ -16,9 +17,10 @@ public class TUserServiceImpl implements TUserService {
 
     @Autowired
     private TUserMapper userMapper;
-
+    @Transactional
     @Override
     public TUser selectByPrimaryKey(Integer id) {
+        TUser tUser = userMapper.selectByPrimaryKey(id);
         return userMapper.selectByPrimaryKey(id);
     }
 }
